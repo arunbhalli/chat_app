d5a8ea02f9c3e82729504169fda2285f05dd0eec
arunbhalli <tomarbhalli@gmail.com>
2021-09-25 12:31:24 +0200

    re-initialized fire base project and readding firebase and firebase-tools

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
-var config = {
-  apiKey: 'AIzaSyCkjKmQ1bYeRmJ-mxrexTi2D7cRvbySuX8',
-  authDomain: 'chat-web-app-b46e2.firebaseapp.com',
-  projectId: 'chat-web-app-b46e2',
-  storageBucket: 'chat-web-app-b46e2.appspot.com',
-  messagingSenderId: '743473564359',
-  appId: '1:743473564359:web:dbfb81a0c0190033d50ce2',
-};
+// import 'firebase/auth';
+// import 'firebase/database';
+var config =  {
+  apiKey: "AIzaSyCqVs4xA2sB-YR52-uQc2fT7QVWxORIOcM",
+  authDomain: "apna-chat-app-f4816.firebaseapp.com",
+  databaseURL: "https://apna-chat-app-f4816-default-rtdb.firebaseio.com",
+  projectId: "apna-chat-app-f4816",
+  storageBucket: "apna-chat-app-f4816.appspot.com",
+  messagingSenderId: "879690433716",
+  appId: "1:879690433716:web:33c68f043cf20f254f83e1",
+  measurementId: "G-G4XP65H2EV"
+}
 const app = firebase.initializeApp(config);
-export const auth = app.auth();
-export const database = app.database();
+// export const auth = app.auth();
+// export const database = app.database();
