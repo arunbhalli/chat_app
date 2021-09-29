65c38bef95dc9e464a7aa5cecafdc2379506d04d
arunbhalli <tomarbhalli@gmail.com>
2021-09-29 17:19:56 +0200

    fixed aerror of google login

--- src/component/Signin.jsx
+++ src/component/Signin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 import { Container, Grid, Row, Panel, Col, Button, Icon, Alert } from 'rsuite';
 import { auth, database } from '../misc/firebase';
 const Signin = () => {
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/app';
-// import 'firebase/auth';
-// import 'firebase/database';
-var config =  {
-  apiKey: "AIzaSyCqVs4xA2sB-YR52-uQc2fT7QVWxORIOcM",
-  authDomain: "apna-chat-app-f4816.firebaseapp.com",
-  databaseURL: "https://apna-chat-app-f4816-default-rtdb.firebaseio.com",
-  projectId: "apna-chat-app-f4816",
-  storageBucket: "apna-chat-app-f4816.appspot.com",
-  messagingSenderId: "879690433716",
-  appId: "1:879690433716:web:33c68f043cf20f254f83e1",
-  measurementId: "G-G4XP65H2EV"
-}
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+var config = {
+  apiKey: 'AIzaSyCqVs4xA2sB-YR52-uQc2fT7QVWxORIOcM',
+  authDomain: 'apna-chat-app-f4816.firebaseapp.com',
+  databaseURL: 'https://apna-chat-app-f4816-default-rtdb.firebaseio.com',
+  projectId: 'apna-chat-app-f4816',
+  storageBucket: 'apna-chat-app-f4816.appspot.com',
+  messagingSenderId: '879690433716',
+  appId: '1:879690433716:web:33c68f043cf20f254f83e1',
+  measurementId: 'G-G4XP65H2EV',
+};
 const app = firebase.initializeApp(config);
-// export const auth = app.auth();
-// export const database = app.database();
+export const auth = app.auth();
+export const database = app.database();
